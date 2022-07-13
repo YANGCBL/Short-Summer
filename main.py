3d7393d6dedf94818a6d254806a82fe4fe950739
YANGCBL <1120429447@qq.com>
2022-07-13 17:19:25 +0800

    the third test

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import torch
+import numpy as np
+
+x = torch.ones(2, 2, requires_grad=True)
+
+print(x)
